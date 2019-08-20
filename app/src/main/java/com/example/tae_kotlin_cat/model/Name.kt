0a55164bee6fb56a603d883a3fb96fbccb7724cf
Krom001 <user@example.com>
2019-08-20 10:05:55 +0100

    working  version

--- app/src/main/java/com/example/tae_kotlin_cat/model/Name.kt
+++ app/src/main/java/com/example/tae_kotlin_cat/model/Name.kt
@@ -1,7 +1,4 @@
-package com.example.tae_kotlin_cat.model
-import com.google.gson.annotations.SerializedName
-
-/*
+/* 
 Copyright (c) 2019 Kotlin Data Classes Generated from JSON powered by http://www.json2kotlin.com
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -13,12 +10,8 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 For support, please feel free to contact me at https://www.linkedin.com/in/syedabsar */
 
 
-data class All (
+data class Name (
 
-	@SerializedName("_id") val _id : String,
-	@SerializedName("text") val text : String,
-	@SerializedName("type") val type : String,
-	@SerializedName("user") val user : User,
-	@SerializedName("upvotes") val upvotes : Int,
-	@SerializedName("userUpvoted") val userUpvoted : String
+	@SerializedName("first") val first : String,
+	@SerializedName("last") val last : String
 )
